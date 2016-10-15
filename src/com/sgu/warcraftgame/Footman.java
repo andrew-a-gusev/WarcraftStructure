5be4a8395e4c8424146429a260aca685123828dc
UAndreyV <andreyusa08@gmail.com>
2016-10-15 21:33:45 +0300

    added a small implementation Battle:)

--- src/com/sgu/warcraftgame/Footman.java
+++ src/com/sgu/warcraftgame/Footman.java
@@ -28,4 +28,5 @@ public class Footman extends HumansUnit {
     }
 
 
+
 }
--- src/com/sgu/warcraftgame/TestWarcraft.java
+++ src/com/sgu/warcraftgame/TestWarcraft.java
@@ -1,17 +1,32 @@
 package com.sgu.warcraftgame;
 
 public class TestWarcraft {
-    public static void main(String[] args) {
-        HumansUnit footman=new Footman("Footman1",100,100,50);//  simple creation
-        HumansUnit archer=new Archer("Archer1",50,50,35);
+    public static void main(String[] args) throws InterruptedException {
+        HumansUnit footman = new Footman("Solder", 1000, 100, 50);//  simple creation
+        HumansUnit archer = new Archer("Ranger", 500, 50, 35);
 
-        HumanConstruction humanConstruction=new Barracks();
-        HumansUnit footman1=humanConstruction.createUnits("footman"); //creation using barracks
-        humanConstruction.upgrade("Swords");
-        footman1.setHp(150);
+        HumanConstruction humanConstruction = new Barracks();
+        //HumansUnit footman1 = humanConstruction.createUnits("footman"); //creation using barracks
+        // HumansUnit archer1 = humanConstruction.createUnits("archer"); //creation using barracks
+
+
+        //humanConstruction.upgrade("Swords");
+       /* footman1.setHp(150);
         footman1.setMp(150);
-        System.out.println(footman1.getHp());
-        footman1.attack(archer);
+        */
+        //   System.out.println(footman1.getHp());
+        while (footman.getHp() > 0 && archer.getHp() > 0) {
+            footman.attack(archer);
+            archer.setHp(archer.getHp() - footman.getDamage());
+            archer.attack(footman);
+            footman.setHp(footman.getHp() - archer.getDamage());
+            System.out.println("archer has " + archer.getHp() + " hp");
+            System.out.println("footman has " + footman.getHp() + " hp");
+            Thread.sleep(1000);
+            if (archer.getHp() == 0) System.out.println("footman win!");
+            else
+                System.out.println("archer win!");
+        }
 
     }
 
